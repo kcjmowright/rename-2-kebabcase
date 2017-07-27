b74beb808a0ac9a798c3d76caaac56ef4389a53b
Justin Wright <justin.wright@firemon.com>
2017-07-26 20:43:53 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+rename-2-kebabcase
+==================
+
+## Prerequisites
+
+Node 6.x
+NPM 3.x
+
+## Build 
+
+```sh
+git clone https://github.com/kcjmowright/rename-2-kebabcase.git
+npm install
+```
+
+## Run
+
+```sh
+node rename-2-kebabcase -r dir1 path-to-dir2
+```
+
+### Options
+
+- `-r` Recursive
+- paths to one or more directories.
+
+ 
+
---
+++ index.js
@@ -0,0 +1,60 @@
+let _ = require('lodash');
+let process = require('process');
+let recursive = false;
+let directories = []; 
+let fs = require('fs');
+
+process.argv.forEach((val, idx) => {
+  if(idx < 2) {
+    return;
+  }
+  console.log(`argument ${idx}: ${val}`);
+  switch(val) {
+    case '-r':
+      recursive = true;
+      break;
+    default:
+      directories.push(val);
+      break;
+  } 
+});
+
+if(!directories.length) {
+  directories.push(process.cwd());
+}
+directories.forEach(dir => processDirectory(dir));
+
+function processDirectory(dir) {
+  console.log(`Reading contents of "${dir}"...`);
+  fs.readdir(dir, (err, files) => {
+    if(_.isUndefined(files) || !files.length) {
+      console.log(`no files found in ${dir}`);
+      return;
+    }
+    files.forEach(f => {
+      let idx = f.lastIndexOf('.');
+      let renamed;
+      let stats = fs.statSync(`${dir}/${f}`);
+      let suffix;
+       
+      if(stats.isDirectory()) {
+        processDirectory(`${dir}/${f}`);
+        return;
+      } else if(!stats.isFile()) {
+        return;
+      }
+      if(idx >= 0) {
+        suffix = f.substring(f.lastIndexOf('.'));
+        renamed = `${_.kebabCase(f.substring(0, idx))}${suffix}`;
+      } else {
+        renamed = _.kebabCase(f);
+      }
+      console.log(`Attempting to rename "${f}" to "${renamed}"`);
+      fs.rename(`${dir}/${f}`, `${dir}/${renamed}`, e => {
+        if(e) {
+          console.log(e);
+        }
+      });
+    });
+  });
+}
